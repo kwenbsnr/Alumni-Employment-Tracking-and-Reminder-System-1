077226817989f2a75e8d9fa58d667a5562309f40
marian_marchan <marchanmayang687@gmail.com>
2025-11-12 18:45:52 +0800

    idk

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@ $db   = "alumtrak_experiment";
 
 //$conn = new mysqli($host, $user, $pass, $db);
 //$conn = new mysqli("127.0.0.1:3307", "root", "", "alumtrak_experiment");
-$conn = new mysqli("127.0.0.1", "root", "", "alumtrak_experiment", 3306);
+$conn = new mysqli ("127.0.0.1", "root", "", "alumtrak_experiment", 3306);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
